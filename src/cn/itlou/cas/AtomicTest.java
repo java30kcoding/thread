aa331f3ba7f5f633c0d1bc79a188c619871e85db
yuanyulou <434356430@qq.com>
2019-05-28 20:49:58 +0800

    多线程学习

---
+++ src/cn/itlou/cas/AtomicTest.java
@@ -0,0 +1,24 @@
+package cn.itlou.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Atomic测试
+ */
+public class AtomicTest {
+
+    public static void main(String[] args) throws InterruptedException{
+        //自增
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    atomicInteger.incrementAndGet();
+                }
+            }).start();
+        }
+        Thread.sleep(2000L);
+        System.out.println(atomicInteger.get());
+    }
+
+}
